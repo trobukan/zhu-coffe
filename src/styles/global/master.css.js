173f85b920282624a925e374bf7fdd5073dce072
trobukan <trobukan@gmail.com>
2025-09-23 15:51:17 -0300

    [fix] Accordion footer

--- src/styles/global/master.css.js
+++ src/styles/global/master.css.js
@@ -3,9 +3,9 @@ window.masterCSSConfig = {
     // > Colors
     secondary: "#4a2e19ff",
     primary: "#00804bff",
-    "unselected-card-color": "#eeeeeeff",
+    ["unselected-card-color"]: "#eeeeeeff",
     // > Card Shadow
-    "layered-shadow": `0 1px 1px hsl(0deg 0% 0% / 0.075),
+    ["layered-shadow"]: `0 1px 1px hsl(0deg 0% 0% / 0.075),
     0 2px 2px hsl(0deg 0% 0% / 0.075),
     0 4px 4px hsl(0deg 0% 0% / 0.075),
     0 8px 8px hsl(0deg 0% 0% / 0.075),
@@ -18,9 +18,9 @@ window.masterCSSConfig = {
     "list": 'gap:0.5rem p:0 list-style:none color:white>li>a text-decoration:none>li>a',
 
     "accordion": "flex flex-col jc:start r:0.25rem ai:start w:100%@<sm p:0.25rem|1rem@<sm cursor:pointer@<sm ",
-    "accordion__header": "flex jc:space-between w:100% p:0px p:0.5rem|1rem fg:white r:0.5rem@<sm",
-    "accordion__content": "overflow:hidden h:auto transition:all 0.3s ease flex-col@<sm jc:start ai:start@<sm",
-    "accordion__icon": "w:24px transition:all 0.3s display:none@>=sm",
-    "accordion--collapsed": "h:0 hidden@<sm",
+    "accordion-header": "flex jc:space-between w:100% p:0px p:0.5rem|1rem fg:white r:0.5rem@<sm",
+    "accordion-content": "overflow:hidden h:auto transition:all 0.3s ease flex-col@<sm jc:start ai:start@<sm",
+    "accordion-collapsed": "h:0 hidden@<sm",
+    "accordion-icon": "w:24px transition:all 0.3s display:none@>=sm"
   }
 }
