0eb21da7b224f57a7fd217b2261189d216b9311d
trobukan <trobukan@gmail.com>
2025-09-23 15:22:30 -0300

    [feat] Responsive accordion footer

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+if (window.innerWidth < 768) {
+  document.querySelectorAll('.accordion').forEach((acc) => {
+    const content = acc.querySelector('.accordion-content');
+    const icon = acc.querySelector('.accordion-icon');
+
+    acc.addEventListener('click', () => {
+      const isCollapsed = content.classList.contains('accordion-collapsed');
+      content.classList.toggle('accordion-collapsed', !isCollapsed);
+
+      icon.style.transform = isCollapsed ? 'rotate(180deg)' : 'rotate(0deg)';
+    });
+  });
+}
--- src/styles/global/master.css.js
+++ src/styles/global/master.css.js
@@ -1,11 +1,26 @@
 window.masterCSSConfig = {
   variables: {
+    // > Colors
     secondary: "#4a2e19ff",
-    primary: "#00804bff"
+    primary: "#00804bff",
+    ["unselected-card-color"]: "#eeeeeeff",
+    // > Card Shadow
+    ["layered-shadow"]: `0 1px 1px hsl(0deg 0% 0% / 0.075),
+    0 2px 2px hsl(0deg 0% 0% / 0.075),
+    0 4px 4px hsl(0deg 0% 0% / 0.075),
+    0 8px 8px hsl(0deg 0% 0% / 0.075),
+    0 16px 16px hsl(0deg 0% 0% / 0.075)`
   },
   components: {
-    ["card-coffe"]: 'bg:primary rel flex jc:center r:1.25rem w:250px p:2rem|0.25rem',
-    ["findus__list"]: 'flex flex-col gap:0.5rem p:0 list-style:none color:white>li>a text-decoration:none>li>a',
-    ["btn"]: 'bg:white fg:secondary p:0.5rem|1rem r:0.5rem text-decoration:none'
+    "card-coffe": 'bg:primary rel flex flex-col jc:space-between r:1.25rem w:200px p:2rem box-shadow:layered-shadow',
+    "card__image": 'abs top:0 left:50% translate(-50%,-70%)',
+    "btn": 'bg:white fg:secondary p:0.5rem|1rem r:0.5rem text-decoration:none text-align:center',
+    "list": 'gap:0.5rem p:0 list-style:none color:white>li>a text-decoration:none>li>a',
+
+    "accordion": "flex flex-col jc:start r:0.25rem ai:start w:100%@<sm p:0.25rem|1rem@<sm cursor:pointer@<sm ",
+    "accordion-header": "flex jc:space-between w:100% p:0px p:0.5rem|1rem fg:white r:0.5rem@<sm",
+    "accordion-content": "overflow:hidden h:auto transition:all 0.3s ease flex-col@<sm jc:start ai:start@<sm",
+    "accordion-collapsed": "h:0 hidden@<sm",
+    "accordion-icon": "w:24px transition:all 0.3s display:none@>=sm"
   }
 }
