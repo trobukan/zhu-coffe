4c6a62e73ba1efd353aae9e5abf7d585bf308da7
trobukan <trobukan@gmail.com>
2025-09-23 15:28:20 -0300

    [feat] Rename components

--- src/styles/global/master.css.js
+++ src/styles/global/master.css.js
@@ -18,9 +18,9 @@ window.masterCSSConfig = {
     "list": 'gap:0.5rem p:0 list-style:none color:white>li>a text-decoration:none>li>a',
 
     "accordion": "flex flex-col jc:start r:0.25rem ai:start w:100%@<sm p:0.25rem|1rem@<sm cursor:pointer@<sm ",
-    "accordion-header": "flex jc:space-between w:100% p:0px p:0.5rem|1rem fg:white r:0.5rem@<sm",
-    "accordion-content": "overflow:hidden h:auto transition:all 0.3s ease flex-col@<sm jc:start ai:start@<sm",
-    "accordion-collapsed": "h:0 hidden@<sm",
-    "accordion-icon": "w:24px transition:all 0.3s display:none@>=sm"
+    "accordion__header": "flex jc:space-between w:100% p:0px p:0.5rem|1rem fg:white r:0.5rem@<sm",
+    "accordion__content": "overflow:hidden h:auto transition:all 0.3s ease flex-col@<sm jc:start ai:start@<sm",
+    "accordion__icon": "w:24px transition:all 0.3s display:none@>=sm",
+    "accordion--collapsed": "h:0 hidden@<sm",
   }
 }
