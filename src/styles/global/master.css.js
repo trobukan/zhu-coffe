dd3a4c23c3fca55009a3f30e9a2f2aa578c355e8
trobukan <trobukan@gmail.com>
2025-09-23 12:24:25 -0300

    [feat] Change all color to fg and put color variables  on Mastercss config

--- src/styles/global/master.css.js
+++ src/styles/global/master.css.js
@@ -1,6 +1,11 @@
 window.masterCSSConfig = {
+  variables: {
+    secondary: "#4a2e19ff",
+    primary: "#00804bff"
+  },
   components: {
-    ["card-coffe"]: 'bg:$(primary-color) rel flex flex-col jc:center ai:center r:1.25rem w:250px p:2rem|0.25rem',
-    ["findus__list"]: 'flex flex-col gap:0.5rem p:0 list-style:none color:white>li>a text-decoration:none>li>a'
+    ["card-coffe"]: 'bg:primary rel flex jc:center r:1.25rem w:250px p:2rem|0.25rem',
+    ["findus__list"]: 'flex flex-col gap:0.5rem p:0 list-style:none color:white>li>a text-decoration:none>li>a',
+    ["btn"]: 'bg:white fg:secondary p:0.5rem|1rem r:0.5rem text-decoration:none'
   }
 }
